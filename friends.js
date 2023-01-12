da61c2392317395ba0ee78023bf9c0222e2e22f3
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-11 22:10:29 -0500

    Add update to friends

--- friends.js
+++ friends.js
@@ -3,3 +3,10 @@ console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
 
+var places = ["france", "London", "Rome", "NYC", "NJ"]
+places.push();
+console.log(places);
+places.slice();
+console.log(places);
+places.pop();
+console.log(places);
